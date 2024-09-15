e242db8167ff444bb04dbf2668dcf9f1b0515924
0xEodum <miraiff0@gmail.com>
2024-09-15 17:17:31 +0300

    Пункт 4. Создание валидаторов, геттеров и сеттеров для класса сущности

--- LW1/src/main/java/org/lw1base/Doctor.java
+++ LW1/src/main/java/org/lw1base/Doctor.java
@@ -22,4 +22,75 @@ public class Doctor {
         this.qualification = qualification;
         this.specialtyId = specialtyId;
     }
+
+    public static Doctor createDoctor(int doctorId, String lastName, String firstName, String middleName, int qualification, int specialtyId) {
+        validateDoctorId(doctorId);
+        validateName(lastName, "Last name");
+        validateName(firstName, "First name");
+        validateName(middleName, "Middle name");
+        validateQualification(qualification);
+        validateSpecialtyId(specialtyId);
+
+        return new Doctor(doctorId, lastName, firstName, middleName, qualification, specialtyId);
+    }
+
+    private static void validateDoctorId(int doctorId) {
+        if (doctorId <= 0) {
+            throw new IllegalArgumentException("ID врача должно быть целым положительным числом");
+        }
+    }
+
+    private static void validateName(String name, String fieldName) {
+        if (name == null || name.isEmpty() || !NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(fieldName + " должно включать только буквы, дефисы и быть не пустым");
+        }
+    }
+
+    private static void validateQualification(int qualification) {
+        if (qualification < MIN_QUALIFICATION || qualification > MAX_QUALIFICATION) {
+            throw new IllegalArgumentException("Квалификация должна быть между " + MIN_QUALIFICATION + " и " + MAX_QUALIFICATION);
+        }
+    }
+
+    private static void validateSpecialtyId(int specialtyId) {
+        if (specialtyId <= 0) {
+            throw new IllegalArgumentException("ID специальности должно быть целым положительным числом");
+        }
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getQualification() {
+        return qualification;
+    }
+
+    public int getSpecialtyId() {
+        return specialtyId;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "doctorId=" + doctorId +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", qualification=" + qualification +
+                ", specialtyId=" + specialtyId +
+                '}';
+    }
 }
--- LW1/src/main/java/org/lw1base/Main.java
+++ LW1/src/main/java/org/lw1base/Main.java
@@ -1,8 +1,7 @@
 package org.lw1base;
 
-
 public class Main {
     public static void main(String[] args) {
-
+        
     }
 }
